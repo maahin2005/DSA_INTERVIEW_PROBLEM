321078b78c1da5fcee04e6f8c3693e86d66f2c15
Sangeeta <sangeeta136428sharma@gmail.com>
2024-08-16 09:09:58 +0530

    stack problems added

---
+++ Stack/1.py
@@ -0,0 +1,25 @@
+# 1. Valid Parentheses
+# Problem:
+# Given a string containing just the characters (, ), {, }, [ and ], determine if the input string is valid.
+
+def is_valid(s):
+    stack=[]
+    for i in s:
+        if i=='(' or i=='[' or i=='{':
+            stack.append(i)
+        elif stack and (stack[-1]=='(' and i==')' or stack[-1]=='{' and i=='}' or stack[-1]=='[' and i==']'):
+            stack.pop()
+        else:
+            return False
+        
+    if stack:
+        return False
+    else:
+        return True
+
+
+
+
+
+s = "()[]{}"
+print(is_valid(s))  # Output: True
