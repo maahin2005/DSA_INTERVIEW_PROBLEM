90f79333287f65f86095a212ec5ddce780f0a868
Sangeeta <sangeeta136428sharma@gmail.com>
2025-01-16 23:58:25 +0530

    day16

---
+++ dsa-100-days/1.Array/Easy/Q16.py
@@ -0,0 +1,10 @@
+def BubbleSort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+
+arr=[4,1,7]
+BubbleSort(arr)
+print(arr)
