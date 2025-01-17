46549b157b842c07f334e900a674643d9ac1b984
Sangeeta <sangeeta136428sharma@gmail.com>
2025-01-17 23:51:29 +0530

    day17

---
+++ dsa-100-days/1.Array/Easy/Q17(TwoPointer).py
@@ -0,0 +1,17 @@
+# Two Sum Problem 
+# 🌟 Problem Statement:
+# You are given a sorted array of integers. Your task is to find two numbers such that their sum equals a given target. Use the two-pointer technique to solve this efficiently.
+
+
+
+# Practice Challenge:
+
+# 🌟 Problem Statement:
+
+# Given a sorted array, determine if there exist two numbers such that their difference equals a given value.
+
+# Example:
+
+# Input: arr = [1, 3, 5, 7, 9], diff = 4
+
+# Output: [1, 5]
